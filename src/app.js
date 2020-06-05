e98a4841ff995de47755c5ccf296943e1fafa00c
MiPersona <bgva2005@yahoo.com>
2020-06-05 14:09:18 -0500

    0004_Formik Forma abreviada y con manejo de errores

--- src/app.js
+++ src/app.js
@@ -1,39 +1,53 @@
 import React from "react";
-import { withFormik } from "formik";
+import { withFormik, Field } from "formik";
 
 function MyForm(props) {
-  const {
-    handleSubmit,
-    isSubmitting,
-    handleChange,
-    handleBlur,
-    values,
-  } = props;
+  const { isSubmitting, isValid, handleSubmit, errors, touched } = props;
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        name="email"
-        type="email"
-        onChange={handleChange}
-        onBlur={handleBlur}
-        value={values.email}
-      />
-      <input
-        name="password"
-        type="password"
-        onChange={handleChange}
-        onBlur={handleBlur}
-        value={values.password}
-      />
-      <button type="submit" disabled={isSubmitting}>
-        Submit
-      </button>
+      <div className="row">
+        Email:
+        <Field name="email" type="email" className="input" />
+        {errors.email && touched.email && (
+          <div className="error">{errors.email}</div>
+        )}
+      </div>
+
+      <div className="row">
+        Password:
+        <Field name="password" type="password" className="input" />
+        {errors.password && touched.password && (
+          <div className="error">{errors.password}</div>
+        )}
+      </div>
+
+      <div className="row">
+        <button
+          type="submit"
+          className={`submit ${isSubmitting || !isValid ? "disabled" : ""}`}
+          disabled={isSubmitting || !isValid}
+        >
+          Submit
+        </button>
+      </div>
     </form>
   );
 }
 
 export default withFormik({
+  validate(values) {
+    const errors = {};
+
+    if (!values.password) {
+      errors.password = "Passwrod is required";
+    } else if (values.password.length < 8) {
+      errors.password = "Passwrod must be at least 8 caracters";
+    }
+
+    return errors;
+  },
+
   handleSubmit(values, formikBag) {
     console.log(values);
 
