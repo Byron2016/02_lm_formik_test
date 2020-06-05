4cda45e3f92345e32e61ad914cb0cec0204dc297
MiPersona <bgva2005@yahoo.com>
2020-06-05 13:17:47 -0500

    0003_Primera Forma con Formik

--- src/app.js
+++ src/app.js
@@ -1,13 +1,42 @@
 import React from "react";
+import { withFormik } from "formik";
 
 function MyForm(props) {
+  const {
+    handleSubmit,
+    isSubmitting,
+    handleChange,
+    handleBlur,
+    values,
+  } = props;
+
   return (
-    <form>
-      <input name="email" type="email" />
-      <input name="password" type="password" />
-      <button type="submit">Submit</button>
+    <form onSubmit={handleSubmit}>
+      <input
+        name="email"
+        type="email"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        value={values.email}
+      />
+      <input
+        name="password"
+        type="password"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        value={values.password}
+      />
+      <button type="submit" disabled={isSubmitting}>
+        Submit
+      </button>
     </form>
   );
 }
 
-export default MyForm;
+export default withFormik({
+  handleSubmit(values, formikBag) {
+    console.log(values);
+
+    formikBag.setSubmitting(false);
+  },
+})(MyForm);
