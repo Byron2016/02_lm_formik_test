310fbe3d0600b95d81ec9792b80962e218c9eb14
MiPersona <bgva2005@yahoo.com>
2020-06-05 13:02:05 -0500

    0002_Primera Forma

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
 
 function MyForm(props) {
-	return (
-		<div>
-			hola
-		</div>
-	);
-
+  return (
+    <form>
+      <input name="email" type="email" />
+      <input name="password" type="password" />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default MyForm;
